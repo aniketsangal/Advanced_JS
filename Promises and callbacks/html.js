9f643e583c3c001add11b748bcc63b073125d335
XYZ <XYZ@example.cogit config --global user.email>
2026-08-24 09:42:20 +0530

    new commit

--- Promises and callbacks/html.js
+++ Promises and callbacks/html.js
@@ -1,4 +1,4 @@
-const promise = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
     let success =true
     if(success){
         resolve({
@@ -11,10 +11,33 @@ const promise = new Promise((resolve, reject) => {
     }
 })
 
-promise
+promise1
 .then(Response => {
     console.log(Response)
 })
 .catch(error => {
     console.log(error)
 })
+
+const promise2 = new Promise((resolve, reject) => {
+    let success = true
+    if(success){
+        resolve({
+            id: 76347497648,
+            username: "Zohn Doe"
+        })
+    }
+    else{
+        reject(new Error("User not found"))
+    }
+})
+
+promise2.then(response => console.log(response)).catch(error => console.log(error.message))
+
+Promise.all([promise1, promise2])
+    .then((responses) => {
+        console.log(responses);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
