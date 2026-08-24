fa743c7e8a988f237712352193c60c874ef84c00
XYZ <XYZ@example.cogit config --global user.email>
2026-08-24 10:13:43 +0530

    update

---
+++ Promises and callbacks/index.js
@@ -0,0 +1,90 @@
+const promise1 = new Promise((resolve, reject) => {
+  let success = true;
+  if (success) {
+    resolve({
+      id: 2930309,
+      username: "John Doe",
+    });
+  } else {
+    reject(new Error("User not found"));
+  }
+});
+
+promise1
+  .then((Response) => {
+    console.log(Response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  let success = true;
+  if (success) {
+    resolve({
+      id: 76347497648,
+      username: "John Doe",
+    });
+  } else {
+    reject(new Error("User not found"));
+  }
+});
+
+promise2
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error.message));
+
+Promise.all([promise1, promise2])
+  .then((responses) => {
+    console.log(responses);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+Promise.race([promise1, promise2])
+  .then((responses) => {
+    console.log(responses);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+Promise.allSettled([promise1, promise2])
+  .then((responses) => {
+    console.log(responses);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+Promise.any([promise1, promise2])
+  .then((responses) => {
+    console.log(responses);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+async function fetchData() {
+    return new Promise((resolve, reject) => {
+        let success = true;
+        if (success) {
+            resolve({
+                id: 73676463866487,
+                username: 'John Doe'
+            })
+        }
+        else {
+            reject(new Error("Data not Fetched"))
+        }
+    })
+}
+
+async function getUser(){
+    const user = await fetchData()
+    console.log(user)
+}
+
+getUser()
+
